cb46c56aa30c958097012d62536fa83daa172fd3
akinleyeOJ <akinleyeoj@gmail.com>
2025-10-08 19:33:28 +0200

    chore: update home

--- app/(tabs)/Home.tsx
+++ app/(tabs)/Home.tsx
@@ -1,18 +1,39 @@
 import React, {useState} from "react";
-import { View, StyleSheet, Alert, Text } from "react-native";
+import { View, StyleSheet, Alert, Text, ScrollView } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import CustomButton from "../../components/Button";
 import {ProductCard, RecentlyViewedCard} from "../../components/Card";
+import HomeHeader from "../../components/HomeHeader";
+import { recentlyViewedProducts, forYouProducts, hotProducts } from "../../data/mockProducts";
+
 
 const Home = () => {
-  const handleButtonPress = () => {
-    Alert.alert("Button Pressed");
+  const [likedProducts, setLikedProducts] = useState<number[]>([]);
+
+  const handleLike = (productId: number) => {
+    setLikedProducts((prev) => prev.includes(itemId)
+      ? prev.filter((id) => id !== itemId) : [...prev, itemId]
+    );
   };
 
+  const handleProductPress = (productId: number) => {
+    console.log(`Product ${productId} pressed`);
+  };
+
+
   return (
-    <View style={styles.container}>
-      <Text>Home</Text>
-    </View>
+   <SafeAreaView style={styles.container}>
+    {/* Home Header section*/}
+    <HomeHeader userName="John Doe" userAvatar="https://via.placeholder.com/150" />
+    
+    <ScrollView showsVerticalScrollIndicator={false}>
+    {/* Recently Viewed section*/}
+
+    </ScrollView>
+    
+
+    
+   </SafeAreaView>
   );
 };
 
