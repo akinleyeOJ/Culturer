bc342e94074f16507b8a8526dc67ad4b612b09c0
akinleyeOJ <akinleyeoj@gmail.com>
2025-10-13 04:48:31 +0200

    fix: remove home header

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -8,6 +8,7 @@ export default function TabLayout() {
         name="Home"
         options={{
           title: 'Home',
+          headerShown: false,
           tabBarIcon: ({ color }) => <FontAwesome size={28} name="home" color={color} />,
         }}
       />
