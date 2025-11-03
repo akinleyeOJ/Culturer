9b4c7c2cccd7387fdbb2409a7247dda425838183
akinleyeOJ <akinleyeoj@gmail.com>
2025-11-03 02:59:41 +0100

    Fix: linting issue

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,45 +1,54 @@
-import FontAwesome from '@expo/vector-icons/FontAwesome';
-import { Tabs } from 'expo-router';
+import FontAwesome from "@expo/vector-icons/FontAwesome";
+import { Tabs } from "expo-router";
 
 export default function TabLayout() {
   return (
-    <Tabs screenOptions={{ tabBarActiveTintColor: 'blue' }}
-    >
+    <Tabs screenOptions={{ tabBarActiveTintColor: "blue" }}>
       <Tabs.Screen
         name="Home"
         options={{
-          title: 'Home',
+          title: "Home",
           headerShown: false,
-          tabBarIcon: ({ color }) => <FontAwesome size={28} name="home" color={color} />,
+          tabBarIcon: ({ color }) => (
+            <FontAwesome size={28} name="home" color={color} />
+          ),
         }}
       />
       <Tabs.Screen
         name="Browse"
         options={{
-          title: 'Browse',
+          title: "Browse",
           headerShown: false,
-          tabBarIcon: ({ color }) => <FontAwesome size={28} name="search" color={color} />,
+          tabBarIcon: ({ color }) => (
+            <FontAwesome size={28} name="search" color={color} />
+          ),
         }}
       />
       <Tabs.Screen
         name="Wishlist"
         options={{
-          title: 'Wishlist',
-          tabBarIcon: ({ color }) => <FontAwesome size={28} name="heart" color={color} />,
+          title: "Wishlist",
+          tabBarIcon: ({ color }) => (
+            <FontAwesome size={28} name="heart" color={color} />
+          ),
         }}
       />
       <Tabs.Screen
         name="Message"
         options={{
-          title: 'Message',
-          tabBarIcon: ({ color }) => <FontAwesome size={28} name="envelope" color={color} />,
+          title: "Message",
+          tabBarIcon: ({ color }) => (
+            <FontAwesome size={28} name="envelope" color={color} />
+          ),
         }}
       />
       <Tabs.Screen
         name="Profile"
         options={{
-          title: 'Profile',
-          tabBarIcon: ({ color }) => <FontAwesome size={28} name="user" color={color} />,
+          title: "Profile",
+          tabBarIcon: ({ color }) => (
+            <FontAwesome size={28} name="user" color={color} />
+          ),
         }}
       />
     </Tabs>
