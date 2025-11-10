e73b7168a5e43e071ac9d98cb4b71ee7adac9408
akinleyeOJ <akinleyeoj@gmail.com>
2025-11-10 23:06:07 +0100

    chore: update  app layout.tsx with authprovider and redirect login to home.tsx

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,10 +1,13 @@
 import { Stack } from "expo-router";
 import "../global.css";
+import { AuthProvider } from "../contexts/AuthContext";
 
 export default function RootLayout() {
   return (
-    <Stack screenOptions={{ headerShown: false }}>
-      <Stack.Screen name="index" options={{ headerShown: false }} />
-    </Stack>
+    <AuthProvider>
+      <Stack screenOptions={{ headerShown: false }}>
+        <Stack.Screen name="index" options={{ headerShown: false }} />
+      </Stack>
+    </AuthProvider>
   );
 }
