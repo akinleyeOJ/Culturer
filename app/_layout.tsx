a1a9844968f819e59ea344330d0ba6d4fe27e2f9
akinleyeOJ <akinleyeoj@gmail.com>
2025-12-02 07:02:27 +0100

    chore: app linking for filter

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -13,9 +13,17 @@ export default function RootLayout() {
             headerShown: false,
             // 'fade' makes the screen appear without sliding, allowing the keyboard 
             // to animate up from the bottom independently.
-            animation: "fade", 
+            animation: "fade",
             // Optional: You can also use 'none' for an instant appearance
             // animation: "none",
+          }}
+        />
+        <Stack.Screen
+          name="filter"
+          options={{
+            headerShown: false,
+            presentation: 'card',
+            animation: 'slide_from_right'
           }}
         />
         <Stack.Screen name="+not-found" options={{ headerShown: false }} />
